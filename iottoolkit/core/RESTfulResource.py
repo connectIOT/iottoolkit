249a1d76c4d717438b3a2e122d877d2a856c339a
mjkoster <michaeljohnkoster@gmail.com>
2013-12-09 00:08:39 -0800

    serialize object tree to JSON
    /l endpoint does a recursive serialization, including graphs and
    settings of Observers and Agents

--- iottoolkit/core/RESTfulResource.py
+++ iottoolkit/core/RESTfulResource.py
@@ -18,20 +18,48 @@ to content types
 @author: mjkoster
 '''
 from Resource import Resource
+import json
 
 class ResourceList(object):
     def __init__(self, listObject):
+        self._containerClasses = ['SmartObject', 'Observers', 'Agent', 'LinkFormatProxy' , 'ObservableProperty' ]
         self._object = listObject
         self.resources = {}
         
-    def get(self, recursive=None):
+    def getFlat(self):
         self._list = []
         for self._resource in self._object.resources: #only list child objects
             if self._resource not in ('l', 'Properties', 'thisObject', 'baseObject', 'parentObject' ):
                 self._list.append({'resourceName': self._object.resources[self._resource].Properties.get('resourceName'), \
                                    'resourceClass': self._object.resources[self._resource].Properties.get('resourceClass')})
         return self._list
-    
+
+    def get(self):
+        return self._listRecursive(self._object)
+                           
+    def _listRecursive(self, object):
+        resources = object.resources
+        resourceList=[]
+        for resource in resources: #only list child objects
+            if resource not in ('l', 'Properties', 'thisObject', 'baseObject', 'parentObject' ):
+                childObject=resources[resource]
+                resourceName = childObject.Properties.get('resourceName')
+                resourceClass = childObject.Properties.get('resourceClass')
+                resourceConstructor = {'resourceName': resourceName, \
+                                       'resourceClass': resourceClass}
+                if resourceClass in self._containerClasses:
+                    #resourceList.append(resourceConstructor)
+                    #resourceList.append( self._listRecursive(childObject) )# next level
+                    resourceList.append([ resourceConstructor , self._listRecursive(childObject) ] )# next level
+                else:
+                    if resourceClass == 'Description': 
+                        graph = json.loads( childObject.serialize(childObject.get(),'application/json') )
+                        resourceConstructor.update({'graph' : graph })
+                    else:
+                        resourceConstructor.update(childObject.get())
+                    resourceList.append(resourceConstructor)
+        return resourceList
+   
 class RESTfulDictEndpoint(object): # create a resource endpoint from a property reference
     def __init__(self, dictReference):
         self.resources = {}
