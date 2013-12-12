b1a2612cef9d1a7288a4a0bfa436b4c0e5f88345
mjkoster <michaeljohnkoster@gmail.com>
2013-12-12 10:46:28 -0800

    sync
    sync BuildFromModel code

--- examples/BuildFromModels.py
+++ examples/BuildFromModels.py
@@ -2,6 +2,9 @@
 Created on Dec 12, 2013
 
 Create services from a service model instance, create objects from an object model instance 
+How are base objects mapped to services? 
+This file creates a service object that has multiple services and a single object tree
+There could be a service creation service where POSTing this descriptor would 
 
 @author: mjkoster
 '''
@@ -29,6 +32,9 @@ rdflib.plugin.register('json-ld', Parser, 'rdflib_jsonld.parser', 'JsonLDParser'
 rdflib.plugin.register('rdf-json', Serializer, 'rdflib_rdfjson.rdfjson_serializer', 'RdfJsonSerializer')
 rdflib.plugin.register('rdf-json', Parser, 'rdflib_rdfjson.rdfjson_parser', 'RdfJsonParser')
 
+'''
+model format for populating Description and creating SmartObject instances and service instances
+'''
 services = {
     'localHTTP' : {
         'scheme': 'http',
@@ -48,7 +54,18 @@ services = {
         'discovery': '/' 
                     }
              }
-    
+
+"""
+Minimal server needs one base object 
+Starting a server will create one but this provides a discoverable model and container
+
+models = {
+    '/': {
+        'resourceName': '/',
+        'resourceClass': 'SmartObject'
+        },
+    }
+"""    
 models = {
     '/': {
         'resourceName': '/',
@@ -67,32 +84,87 @@ models = {
         'resourceClass': 'ObservableProperty',
         'resourceType': 'temperature',
         'interfaceType':'sensor',
-        'subscribeURI': 'mqtt://smartobjectservice.com:1883/sensors/rhvWeather-01/indoor_temperature',
-        'publishURI': '',
-        'bridgeURI': ''
+        'subscribers': ['mqtt://smartobjectservice.com:1883/sensors/rhvWeather-01/indoor_temperature'],
+        'publishers': '',
+        'bridges': ''
         }
+    }
+
+mqttObserverTemplate = {
+    'resourceName': 'mqttObserver',
+    'resourceClass': 'mqttObserver',
+    'connection': 'localhost',
+    'pubTopic': '',
+    'subTopic': ''
+    }
+
+httpPublisherTemplate = {
+    'resourceName': 'httpPublisher',
+    'resourceClass': 'httpPublisher',
+    'connection': 'localhost'
+    }
+
+httpSubscriberTemplate = {
+    'resourceName': 'httpSubscriber',
+    'resourceClass': 'httpSubscriber',
+    'connection': 'localhost',
+    }
+
+callbackNotifierTemplate = {
+    'resourceName': 'callbackNotifier',
+    'resourceClass': 'callbackNotifier',
+    'handlerURI': 'localhost'
+    }
+
+observerSchemes = ['http', 'coap', 'handler', ]
+
+defaultResources = {
+    'SmartObject': ['Description', 'Agent'],
+    'ObservableProperty': ['Description', 'Observers']
     }
 
 baseObject = None
 
-def objectFromPath(self,path, scope=None):
-    if scope =='parent':
-        return
-    return 
+def objectFromPath(self,path, baseObject):
+    currentObject=baseObject
+    for pathElement in path.split('/')[:-1]:
+        currentObject=object.resources[pathElement]
+    return currentObject
+
+def createByScheme(self, currentResource, resourceDescriptor):
+    pass
+
+def graphFromModel(self, model):
+    pass
+
 
 if __name__ == '__main__' :
     # make models first
-    # make list and sort by path length for import from graph
-    resourceList = models.keys().sortByPathLength()
+    # make list sorted by path length for import from graph, 
+    # could count a split list but this should be the same if we eat slashes somewhere
+    resourceList = sorted( models.keys(), key=str.count('/') )
     
     for resource in resourceList:
         resourceDescriptor = models[resource]
+        # see if base object needs to be created. 
         if resource is '/' and resourceDescriptor['resourceClass'] is 'SmartObject' and baseObject is None:
             baseObject = SmartObject()
         else:
-            objectFromPath(resource, scope ='parent').create(resourceDescriptor)
+            newResource = objectFromPath(resource).create(resourceDescriptor)
+            if resourceDescriptor['resourceClass'] in defaultResources:
+                for defaultResource in defaultResources[resource]:
+                    newChildResource = newResource.create({
+                                        'resourceName': defaultResource,
+                                        'resourceClass': defaultResource
+                                        })
+                    if defaultResource is 'Description': 
+                        newChildResource.set(graphFromModel(resourceDescriptor))
+                    
+                    
             # set description and propagate back up to root
+            newResource.Description.create(resourceDescriptor)
             # make observer instances and agent instances
+            createByScheme(newResource, resourceDescriptor)
             
     
     # make an empty instance of a SmartObject shared by 2 interfaces, 
