64123a9cee2d37a2c5c5a8cd3d457ebbeb02770a
mjkoster <michaeljohnkoster@gmail.com>
2014-10-25 20:03:01 -0700

    draft of notification demo code

--- lwm2m/MdsNotificationDemo.py
+++ lwm2m/MdsNotificationDemo.py
@@ -15,19 +15,20 @@ if __name__ == '__main__' :
     import base64
     
     httpServer = 'http://barista2.cloudapp.net:8080'
-    httpPathBase = '/domain/endpoints'
-    resourceURI = '3302/0/5500'
-    baseURL = httpServer + httpPathBase
-    resourceURL = baseURL + resourceURI
+    httpDomain = 'domain'
+    resourcePathBase = '/' + httpDomain + '/endpoints'
+    subscribeURI = '3302/0/5500'
+    actuateURI = '11101/0/5901'
+    baseURL = httpServer + resourcePathBase
     
     username = 'admin'
     password = 'secret'
     auth = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
     ep_names = []
-
  
     def discoverEndpoints(basePath):
         uriObject = urlparse(basePath)
+        print 'discover: ' + basePath
         httpConnection = httplib.HTTPConnection(uriObject.netloc)
         httpConnection.request('GET', uriObject.path, headers= \
                            {"Accept" : "application/json", "Authorization": ("Basic %s" % auth) })
@@ -40,32 +41,71 @@ if __name__ == '__main__' :
         for endpoint in endpoints:
             if endpoint['type'] == 'presenceDemo':
                 ep_names.append(endpoint['name'])
+                print 'endpoint: ' + endpoint['name']
         return ep_names
-               
 
-    def actuateLEDs(color):
+    def subscribe(resourceURI):
         for ep in ep_names:
-            path = baseURL + '/' + ep + '/11100/0/5900?sync=false'
-            print "PUT: " + color + '  to: ' + path
+            path = httpServer + '/' + httpDomain + '/subscriptions' + '/' + ep + '/' + subscribeURI + '?sync=false'
+            print "subscribe: " + path
             uriObject = urlparse(path)
             httpConnection = httplib.HTTPConnection(uriObject.netloc)
-            httpConnection.request('PUT',   uriObject.path + '?' + uriObject.query, color, \
+            httpConnection.request('PUT',   uriObject.path + '?' + uriObject.query, "", \
                                      {"Content-Type" : "application/json", "Authorization": ("Basic %s" % auth)})
             response = httpConnection.getresponse()
             print response.status, response.reason
             httpConnection.close()
 
- 
+    def poll(channelPath):
+        uriObject = urlparse(channelPath)
+        httpConnection = httplib.HTTPConnection(uriObject.netloc)
+        httpConnection.request('GET', uriObject.path, headers= \
+                           {"Accept" : "application/json", "Authorization": ("Basic %s" % auth) })
+        response = httpConnection.getresponse()
+        print response.status, response.reason
+        if response.status == 200:
+            return json.loads(response.read())
+        print "Closing poll connection"
+        httpConnection.close()
+
+    def actuateLEDbar(ledString = '0000000000'):
+        for ep in ep_names:
+            path = baseURL + ep + actuateURI
+            uriObject = urlparse(path)
+            httpConnection = httplib.HTTPConnection(uriObject.netloc)
+            httpConnection.request('PUT',   uriObject.path + '?' + uriObject.query, ledString, \
+                               {"Content-Type" : "application/json", "Authorization": ("Basic %s" % auth)})
+            response = httpConnection.getresponse()
+            print response.status, response.reason
+            httpConnection.close()    
+    
+    def process_notification(notification):
+        value =  base64.b64decode(notification['payload']) #notification payloads are base64 encoded
+        ledBarString = ""
+        for led in range(10):
+            if value/10 > led:
+                ledBarString += '1'
+            else:
+                ledBarString += '0'
+        actuateLEDbar(ledBarString)
+                
     """
     Start
     """
     print "Started"
 
     discoverEndpoints(baseURL)
+    subscribe(subscribeURI)
     
     try:
         while 1:
-            pass                
+            domainEvents = poll(httpServer + '/' + httpDomain + 'notification/pull')
+            print domainEvents
+            if 'notifications' in domainEvents:
+                for notification in domainEvents['notifications']:
+                    if notification['ep'] in ep_names and notification['path'] == subscribeURI:
+                        process_notification(notification)
+            
     except KeyboardInterrupt: pass
     print 'got KeyboardInterrupt'
     print 'closed'
